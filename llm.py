fb116b5f136f890ce803956bbe6782c68fe2b946
1588mobalmobal <kbh11028@gmail.com>
2025-03-27 19:55:38 +0900

    벡터 데이터베이스 교체 (faiss -> chroma), 일기 삭제 기능 추가,  불필요 파일 제거

--- llm.py
+++ llm.py
@@ -8,7 +8,7 @@ llm_instance = None
 def init_llm():
     global llm_instance
     if llm_instance is None:
-        llm_instance = OllamaLLM(model="exaone3.5:7.8b", temperature=0.1, num_predict=300, format='json')
+        llm_instance = OllamaLLM(model="exaone3.5:7.8b", temperature=0.1, num_predict=256, format='json')
     return llm_instance
 
 def get_log_response(user_input):
@@ -17,14 +17,13 @@ def get_log_response(user_input):
     system_message_prompt = SystemMessagePromptTemplate.from_template(
         '''
     당신은 사용자의 일기를 받아 조언을 해주는 동반자입니다.
-    존댓말을 사용해서 한 문장으로 일기 내용을 평가한 후, 한 문장으로 조언을 해주세요.
-    그 후 일기의 결과를 다음 세가지 분류에 따라 각각 점수를 매겨주세요.
-    체력: 운동활동 미수행 = 0점 / 중간 강도 운동 수행 = 1점 / 강하고 힘든 강도 운동 수행 = 2점
-    지식: 독서/학습활동 미수행 = 0점 / 중간 강도 학습활동 수행 = 1점 / 장시간 고강도 학습활동 수행 = 2점
-    정신력: 정신적-육체적 에너지 미소모 = 0점 / 불편과 번거로움을 이겨냄 = 1점 / 가혹한 환경을 이겨냄 = 2점
-    다음 출력 양식을 지켜주세요.
+    친근한 경어로 한 문장으로 일기 내용을 평가한 후, 한 문장으로 조언을 해주세요.
+    그 후 일기를 다음 세가지 분류에 따라 점수를 매겨주세요.
+    체력: 운동활동 미수행=0점 / 중간 강도 운동 수행=1점 / 강하고 힘든 강도 운동 수행=2점
+    지식: 학습활동 미수행=0점 / 중간 강도 학습활동 수행=1점 / 고강도 학습활동 수행=2점
+    정신력: 정신적-육체적 에너지 미소모=0점 / 불편과 번거로움을 극복=1점 / 가혹한 환경을 극복=2점
+    출력 양식:
     ["격려" : 격려 내용, "조언" : 조언 내용, "체력" : 체력 점수, "지식" : 지식 점수, "정신력" : 정신력 점수]
-    감사합니다.
     '''
     )
     human_message_prompt = HumanMessagePromptTemplate.from_template('{text}')
